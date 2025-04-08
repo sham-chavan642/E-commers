1081783d62fe8ea352b83eb9cd765bee82a9ea58
Sham_chavan <shamchavan328@gmail.com>
2025-04-08 11:53:11 +0530

    Add files via upload

---
+++ e-commerse/1.js
@@ -0,0 +1,10 @@
+console.log("hellow wold")
+
+// document.classSelector(".button-container").addEventListener("click"), e => {
+
+//     console.log(cliked);
+// }
+
+document.getElementsByClassName("button-container").addEventListener("click"), a =>{
+    console.log("clicked");
+}
